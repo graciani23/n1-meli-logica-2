92fc881aadbbed973d1bdeead974ef42ebbc0547
graciani23 <gracianigss@gmail.com>
2019-09-25 14:55:36 -0300

    resolução exercício 4

--- 4_funcoes/exercicio.js
+++ 4_funcoes/exercicio.js
@@ -1,1 +1,38 @@
-//resposta aqui
+
+let r = require('readline-sync');
+let expr = r.question('Infomar qual operacao sera realizada: [somar, subtrair, dividir, multiplicar] ')
+let a = parseFloat(r.question('Informe o primeiro valor: '))
+let b = parseFloat(r.question('Informe o segundo valor: '))
+
+function somar(a, b) {
+    return a + b;
+}
+
+function subtrair(a, b) {
+    return a - b;
+}
+
+function dividir(a, b) {
+    return a /b;
+}
+
+function multiplicar(a, b) {
+    return a * b;
+}
+
+switch (expr) {
+    case 'somar':
+        console.log(`A soma entre ${a} e ${b} é ${somar(a, b)}`);
+        break;
+    case 'subtrair':
+        console.log(`A subtração entre ${a} e ${b} é ${subtrair(a, b)}`);
+        break;
+    case 'dividir':
+        console.log(`A divisão entre ${a} e ${b} é ${dividir(a, b)}`);
+        break;
+    case 'multiplicar':
+        console.log(`A multiplicação entre ${a} e ${b} é ${multiplicar(a, b)}`);
+        break;
+    default:
+        console.log('Operação inválida!');
+}
---
+++ 4_funcoes/exercicio2.js
@@ -0,0 +1,36 @@
+/*
+let x = 1;
+switch(x) {
+    case 1:
+        console.log('Luma');
+        break;
+    case 2:
+        console.log('Graciani');
+        break;
+    case 3:
+        console.log('Silvia');
+        break;
+    default:
+        console.log('Reprograma');
+} */
+
+/*
+let r = require('readline-sync');
+let oper = r.question('Infomar qual operacao sera realizada: [somar, subtrair, multiplicar ou dividir] ').toUpperCase()
+
+let a = 10
+let b = 5
+
+if (oper == 'SOMAR') {
+    console.log(a + b)
+} else if (oper == 'SUBTRAIR') {
+    console.log(a - b)
+} else if (oper == 'DIVIDIR') {
+    console.log(a / b)
+} else if (oper == 'MULTIPLICAR') {
+    console.log(a / b)
+} else {
+    console.log('Operação inválida!')
+}
+*/
+
