90fa3991ea5b7f46c1d4c203954709a61e619133
graciani23 <gracianigss@gmail.com>
2019-09-24 21:56:07 -0300

    resolução exercício 3

--- 2_op_ar/exercicio.js
+++ 2_op_ar/exercicio.js
@@ -1,1 +1,18 @@
 //resposta aqui
+
+//resposta aqui
+
+let r = require('readline-sync')
+let lista = [
+    { produto: 'maçã', preco: 4 },
+    { produto: 'laranja', preco: 5 },
+    { produto: 'arroz', preco: 10.5 }
+]
+let posicao = parseInt(r.question('Escolha um produto entre 0 e 2: '))
+let desconto = parseFloat(r.question('Digite o percentual de desconto entre 0 e 50.10: '))
+let prodSelecionado = lista[posicao]
+let desc = (100 - desconto) / 100
+let tot = (prodSelecionado.preco * desc).toFixed(2)
+
+
+console.log(`O produto ${lista[posicao].produto} com ${desconto}% de desconto é R$${tot}`)
--- 3_estrutura/exercicio.js
+++ 3_estrutura/exercicio.js
@@ -1,1 +1,35 @@
 //resposta aqui
+let r = require('readline-sync')
+
+let lista = [
+    {
+        nome: 'Nescau',
+        tempoDeValidade: 100,
+        congelado: false
+    },
+    {
+        nome: 'Nuggets',
+        tempoDeValidade: 100,
+        congelado: true
+    },
+    {
+        nome: 'Pâo de queijo',
+        tempoDeValidade: 100,
+        congelado: true
+    }
+]
+let posicao = parseInt(r.question('Escolha um produto entre 0 e 2: '))
+let diasParaVenc = parseInt(r.question('Informe quantos dias faltam para o vencimento: (<=100)'))
+let porcentagem = (diasParaVenc * 100) / lista[posicao].tempoDeValidade
+
+ if (porcentagem <= 2) {
+     console.log(`Descartar o produto ${lista[posicao].nome}, muito proximo de vencer`)
+ } else if (lista[posicao].congelado == false && porcentagem <= 10) {
+     console.log(`Desconto!  O produto ${lista[posicao].nome} não congelado e próximo da validade.`)
+ } else if (lista[posicao].congelado == true && porcentagem <= 5) {
+     console.log(`Desconto! O produto ${lista[posicao].nome} é congelado e próximo da validade.`)
+ } else {
+     console.log(` O produto ${lista[posicao].nome} está longe do vencimento.`)
+ }
+
+//
