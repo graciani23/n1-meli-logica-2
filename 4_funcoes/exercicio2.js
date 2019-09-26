5c5941750c93c60c245555da06de77345aa4344f
graciani23 <gracianigss@gmail.com>
2019-09-25 22:11:12 -0300

    atualização exercício4

--- 4_funcoes/exercicio2.js
+++ 4_funcoes/exercicio2.js
@@ -34,3 +34,38 @@ if (oper == 'SOMAR') {
 }
 */
 
+
+let r = require('readline-sync');
+let oper = r.question('Infomar qual operacao sera realizada: [ + / - / % / * ] ')
+let n1 = parseFloat(r.question('Informe o primeiro número: '))
+let n2 = parseFloat(r.question('Informe o segundo número: '))
+function somar(n1, n2) {
+    return n1 + n2;
+}
+function subtrair(n1, n2) {
+    return n1 - n2;
+}
+function dividir(n1, n2) {
+    return n1 / n2;
+}
+function multiplicar(n1, n2) {
+    return n1 * n2;
+}
+
+switch (oper) {
+    case '+':
+        console.log(somar(n1, n2));
+        break;
+    case '-':
+        console.log(subtrair(n1, n2))
+        break;
+    case '%':
+        console.log(dividir(n1, n2))
+        break;
+    case '*':
+        console.log(multiplicar(n1, n2))
+        break;
+    default:
+        console.log('Opção inválida');
+}
+
---
+++ 5_repetição/exercicio.js
@@ -0,0 +1,27 @@
+
+/*1) Crie um programa para printar todos os números pares de 1 a 100.
+    - Números pares são todos os números que dividos por 2 tem resto 0
+        - Qual operador que retorna o resto da divisão ? Vamos usar ele!*/
+/*
+for (i = 0; i <= 100; i++) {
+    if (i % 2 == 0) {
+        console.log(i)
+    }
+}
+*/
+/* 2) Crie um programa gerador de estrelas. O usuário informa um número, e o programa gera uma pirâmide de estrelas no seguinte formato:
+// Ex.: Usuário informa 5 e o programa retorna
+// *
+// * *
+// * * *
+// * * * * 
+// * * * * * 
+- Você pode concatenar uma string usando: + ou += */
+
+let qtde = 5
+for (i=1; i<=qtde; i++) {
+    console.log('a')
+}
+
+
+
